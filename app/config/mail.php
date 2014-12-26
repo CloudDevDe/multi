29573c33fb516fa919c10a7cfcfee9093c1fddf0
CloudDev <info@clouddev.de>
2014-12-26 15:02:09 +0100

    BugFix Confide

--- app/config/mail.php
+++ app/config/mail.php
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'smtp@computech-ob.de', 'name' => 'Laravel'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -67,7 +67,7 @@ return array(
 	|
 	*/
 
-	'encryption' => 'tls',
+	'encryption' => false,
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => smtp@computech-ob.de,
+	'username' => 'smtp@computech-ob.de',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => RemoteCT12$,
+	'password' => 'RemoteCT12$',
 
 	/*
 	|--------------------------------------------------------------------------
